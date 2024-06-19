f7cabbdfe3f5edd3a5ea0866b42986e885aeeff3
rukiwatsuyoidesune <145592092+rukiwatsuyoidesune@users.noreply.github.com>
2024-06-19 09:27:11 +0700

    Add files via upload

---
+++ PrioryTask/PrioryTask/GetTaskCount.cs
@@ -0,0 +1,17 @@
+﻿namespace PrioryTask
+{
+    public class GetTaskCount
+    {
+        private TaskManager taskManager;
+
+        public GetTaskCount(TaskManager taskManager)
+        {
+            this.taskManager = taskManager;
+        }
+
+        public void Execute()
+        {
+            taskManager.GetTaskCount();
+        }
+    }
+}
---
+++ PrioryTask/PrioryTask/ViewNextTask.cs
@@ -0,0 +1,17 @@
+﻿namespace PrioryTask
+{
+    public class ViewNextTask
+    {
+        private TaskManager taskManager;
+
+        public ViewNextTask(TaskManager taskManager)
+        {
+            this.taskManager = taskManager;
+        }
+
+        public void Execute()
+        {
+            taskManager.ViewNextTask();
+        }
+    }
+}
